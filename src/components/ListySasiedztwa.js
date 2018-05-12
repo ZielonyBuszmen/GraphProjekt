4c843e59b19a2dc2c71cb45c7acb1bbe8e081c7c
Krzysztof Komar <krzysztof.komar@gazeta.pl>
2018-05-12 13:27:28 +0200

    Listy sąsiedztwa - poprawki

--- src/components/ListySasiedztwa.js
+++ src/components/ListySasiedztwa.js
@@ -1,44 +1,48 @@
 import React from 'react';
-import { listySasiedztwaChanged, macierzSasiedztwaChanged } from '../actions/actionCreators';
+import { listySasiedztwaChanged } from '../actions/actionCreators';
 import { connect } from 'react-redux';
 import { immutablePush, macierzNaListe } from '../helpers';
 
-
-/*
-Małe info:
- - gdy w komponencie zmieni się jakiś input, trzeba wywołać akcję this.props.listySasiedztwaChanged(lists)
- - lists, przesyłane w parametrze, to listy sąsiedztwa w formacie: [ [0,2], [2], [0,1] ]
- - trzeba wymyśleć, jak to zrobić, by to ładnie wyświetlać
- */
 class ListySasiedztwa extends React.Component {
 
-
-  inputRenderer = (list, node) => {
-    let max = list.length < this.props.matrix.length ? list.length + 1 : list.length;
-    let result = [];
-    for (let i = 0; i < max; i++) {
-      const value = list[i];
-      result = immutablePush(result, <input type="text" value={value} style={{width: '35px'}} onChange={(e) => this.handleChange(e, node, i)}/>)
-    }
-    return result;
-  };
-
-  handleChange = (event, node, listElement) => {
-    const value = parseInt(event.target.value);
-    const lists = macierzNaListe(this.props.matrix);
-    lists[node][listElement] = value;
-    this.props.listySasiedztwaChanged(lists);
-  };
-
+  /**
+   * Renderuje całą listę sąsiedztwa, wraz z inputami i wartościami
+   *
+   * @returns {Array}
+   */
   listRenderer() {
     const {matrix} = this.props;
     const lists = macierzNaListe(matrix);
     let result = [];
     lists.map((connectedList, node) => {
-      result[node] = <span>{node} - {this.inputRenderer(connectedList, node)}<br/></span>;
+      result[node] = <span>{node} - {this.inputRowRenderer(connectedList, node)}<br/></span>;
     });
     return result;
   }
+
+  /**
+   * Renderuje pojedynczy wiersz inputów
+   *
+   * @param list
+   * @param startNode
+   * @returns {Array}
+   */
+  inputRowRenderer = (list, startNode) => {
+    let max = list.length < this.props.matrix.length ? list.length + 1 : list.length;
+    let result = [];
+    for (let i = 0; i < max; i++) {
+      const value = list[i];
+      result = immutablePush(result, <input type="text" value={value} style={{width: '35px'}} onChange={(e) => this.handleChange(e, startNode, i)}/>)
+    }
+    return result;
+  };
+
+  handleChange = (event, startNode, listElement) => {
+    const endNode = parseInt(event.target.value);
+    const lists = macierzNaListe(this.props.matrix);
+    lists[startNode][listElement] = endNode;
+    this.props.listySasiedztwaChanged(lists);
+  };
 
   render() {
     return (

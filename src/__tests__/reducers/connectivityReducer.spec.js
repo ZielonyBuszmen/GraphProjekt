1ad7d49b4847b66be3c9270387c9f9261a7ed165
Krzysztof Komar <krzysztof.komar@gazeta.pl>
2018-06-12 14:47:10 +0200

    Przymiarki do testów (#10)
    * Fix buga - niepoprawne zarządzanie inputem w react
    
    * Przenosiny folderów
    
    * Wywalenie console.loga

---
+++ src/__tests__/reducers/connectivityReducer.spec.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+describe('checkConnectivityOfGraph', function () {
+  it('should empty graph', function () {
+    const graph = [];
+
+  });
+
+
+  it('should return false, when graph is not connected', () => {
+    // tutej test
+    expect(true).toBe(true);
+
+  });
+});
+
+describe('checkStrongConnectivityOfGraph', function () {
+  it('should return false, when graph is not connected', () => {
+    // tutej test
+    expect(true).toBe(false);
+  });
+});
+

5bcf57be4180d6d66e5af47ec55593aed57afeb9
Krzysztof Komar <krzysztof.komar@gazeta.pl>
2018-09-27 16:51:39 +0200

    Struktura describe'ów pod testy

--- src/__tests__/helpers.spec.js
+++ src/__tests__/helpers.spec.js
@@ -1,5 +1,63 @@
-describe('asd', function () {
-  it('should asd', function () {
+describe('createEmptyMatrix', () => {
+  it('should create matrix with null', () => {
     expect(true).toBe(true);
   });
+
+  it('should create matrix with 0', () => {
+    expect(true).toBe(true);
+  });
+
+  it('should create matrix with empty string', () => {
+    expect(true).toBe(true);
+  });
+
+  it('should return empty array when nodes is 0', () => {
+    expect(true).toBe(true);
+  });
+});
+
+describe('copyMatrix', () => {
+
+});
+
+describe('immutablePush', () => {
+
+});
+
+describe('createEmptyArray', () => {
+
+});
+
+describe('macierzNaListe', () => { // todo można zmienić nazwę na eng
+
+});
+
+describe('listaNaMacierz', () => { // todo można zmienic nazwe na eng
+
+});
+
+describe('Stack', () => {
+  it('should getLength', () => {
+    expect(true).toBe(true);
+  });
+
+  it('should push', () => {
+    expect(true).toBe(true);
+  });
+
+  it('should pop', () => {
+    expect(true).toBe(true);
+  });
+
+  it('should peek', () => {
+    expect(true).toBe(true);
+  });
+
+  it('should empty', () => {
+    expect(true).toBe(true);
+  });
+});
+
+describe('changeGraphSize', () => {
+
 });

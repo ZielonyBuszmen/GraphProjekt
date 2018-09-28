36b964676c7672db9e965ff82014777f973c92f4
Krzysztof Komar <krzysztof.komar@gazeta.pl>
2018-09-28 17:03:45 +0200

    kombinowanie z testami

--- src/__tests__/components/BlockMask.spec.js
+++ src/__tests__/components/BlockMask.spec.js
@@ -1,23 +1,19 @@
-// import toJson from 'enzyme-to-json';
-// import BlockMask from "../../components/BlockMask";
-// import React from 'react';
-// import { shallow } from 'enzyme';
-//
+import toJson from 'enzyme-to-json';
+import BlockMask from "../../components/BlockMask";
+import React from 'react';
+import { shallow } from 'enzyme';
+
 // import Enzyme from 'enzyme';
 // import Adapter from 'enzyme-adapter-react-16';
-//
+
 // Enzyme.configure({ adapter: new Adapter() });
 
-describe('asd', function () {
-  it('should asd', function () {
-    
-  });
-});
 
 describe('BlockMask', () => {
   it('should render', function () {
-    // const component = shallow(<BlockMask />);
+    const component = shallow(<BlockMask />);
     // expect(toJson(component)).toMatchSnapshot();
+    expect(true).toBeTruthy();
   });
 });
 

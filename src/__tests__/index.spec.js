6194d3b3b84ac71d25fc06eacce3e8c2fe17eee2
Krzysztof Komar <krzysztof.komar@gazeta.pl>
2018-09-28 16:09:46 +0200

    Dodanie enzyme + plik do testów index.js (glowny komponent)

---
+++ src/__tests__/index.spec.js
@@ -0,0 +1,12 @@
+import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
+
+describe('App', function () {
+  it('should render', function () {
+    // todo
+    // const component = shallow(<MojKomponent />);
+    // expect(toJson(component)).toMatchSnapshot();
+  });
+});
+
+

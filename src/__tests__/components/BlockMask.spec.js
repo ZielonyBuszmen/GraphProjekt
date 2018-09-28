28dff4e135d1fdaff476cd4053ad06fad0410bca
Krzysztof Komar <krzysztof.komar@gazeta.pl>
2018-09-28 17:00:41 +0200

    Próba testu komponentu

--- src/__tests__/components/BlockMask.spec.js
+++ src/__tests__/components/BlockMask.spec.js
@@ -1,1 +1,23 @@
-// todo
+// import toJson from 'enzyme-to-json';
+// import BlockMask from "../../components/BlockMask";
+// import React from 'react';
+// import { shallow } from 'enzyme';
+//
+// import Enzyme from 'enzyme';
+// import Adapter from 'enzyme-adapter-react-16';
+//
+// Enzyme.configure({ adapter: new Adapter() });
+
+describe('asd', function () {
+  it('should asd', function () {
+    
+  });
+});
+
+describe('BlockMask', () => {
+  it('should render', function () {
+    // const component = shallow(<BlockMask />);
+    // expect(toJson(component)).toMatchSnapshot();
+  });
+});
+

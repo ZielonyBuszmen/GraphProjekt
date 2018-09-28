a6d112b38dc78a01bc174bc679667b3ec88981a1
Krzysztof Komar <krzysztof.komar@gazeta.pl>
2018-09-28 15:37:24 +0200

    poprawka literówki w nazwie matrixReducer

--- src/__tests__/reducers/matrixReducer.spec.js
+++ src/__tests__/reducers/matrixReducer.spec.js
@@ -4,34 +4,34 @@ import {
   listySasiedztwaChanged,
   macierzSasiedztwaChanged,
 } from "../../actions/actionCreators";
-import matrixReduxer from "../../reducers/matrixReducer";
+import matrixReducer from "../../reducers/matrixReducer";
 
 describe('matrixReducer', () => {
   it('action type => MACIERZ_SASIEDZTWA_CHANGED', () => {
     const state = [[1, 2], [3, 4]];
     const action = macierzSasiedztwaChanged(1, 1, 12);
-    const result = matrixReduxer(state, action);
+    const result = matrixReducer(state, action);
     expect(result).toEqual([[1, 2], [3, 12]]);
   });
 
   it('action type => LISTY_SASIEDZTWA_CHANGED', () => {
     const state = [];
     const action = listySasiedztwaChanged([[1, 2], [0]]);
-    const result = matrixReduxer(state, action);
+    const result = matrixReducer(state, action);
     expect(result).toEqual([[0, 1, 1], [1, 0]]);
   });
 
   it('action type => CREATE_EMPTY_MATRIX', () => {
     const state = [];
     const action = createEmptyMatrix(2);
-    const result = matrixReduxer(state, action);
+    const result = matrixReducer(state, action);
     expect(result).toEqual([[0, 0], [0, 0]]);
   });
 
   it('action type => CHANGE_GRAPH_SIZE', () => {
     const state = [[5]];
     const action = graphSizeChanged(2);
-    const result = matrixReduxer(state, action);
+    const result = matrixReducer(state, action);
     expect(result).toEqual([[5, 0], [0, 0]]);
   });
 
@@ -40,7 +40,7 @@ describe('matrixReducer', () => {
     const action = {
       type: 'other',
     };
-    const result = matrixReduxer(state, action);
+    const result = matrixReducer(state, action);
     expect(result).toEqual(state);
   });
 });

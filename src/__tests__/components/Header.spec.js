918d29f694ff4b6d42742bf97786334cb404a7aa
Krzysztof Komar <krzysztof.komar@gazeta.pl>
2018-09-29 10:15:27 +0200

    Testy do Header

--- src/__tests__/components/Header.spec.js
+++ src/__tests__/components/Header.spec.js
@@ -2,10 +2,15 @@ import React from 'react';
 import toJson from 'enzyme-to-json';
 import { shallow } from 'enzyme';
 import '../../enzymeConfiguration';
+import { Header } from '../../components/Header';
 
-// todo
-describe('asd', function () {
-  it('should asd', function () {
-    expect(true).toBe(true);
+describe('Home', function () {
+  it('should render', function () {
+    const props = {
+      matrix: ['matrix'],
+      graphSize: 2,
+    };
+    const component = shallow(<Header/>);
+    expect(toJson(component)).toMatchSnapshot();
   });
 });
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,20 +4,22 @@ import {
   Navbar,
   NavbarToggler,
   NavbarBrand,
-  Container } from 'reactstrap';
+  Container
+} from 'reactstrap';
 
-class Header extends React.Component {
+export class Header extends React.Component {
   render() {
-    return(
+    return (
       <div>
         <Navbar light expand="md" style={{'backgroundColor': '#290004'}}>
           <Container>
-              <NavbarBrand style={{'color': '#efe7e7'}} href="/"><b>Silna spójność grafów</b></NavbarBrand>
-          <NavbarToggler />
+            <NavbarBrand style={{'color': '#efe7e7'}} href="/"><b>Silna spójność grafów</b></NavbarBrand>
+            <NavbarToggler/>
           </Container>
         </Navbar>
       </div>
     );
   }
 }
+
 export default Header;

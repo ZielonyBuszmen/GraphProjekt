cdde495c85fa2baa629a373ba76eeb0c0bbc1007
Krzysztof Komar <krzysztof.komar@gazeta.pl>
2018-09-29 10:21:52 +0200

    Testy do App

--- src/__tests__/components/App.spec.js
+++ src/__tests__/components/App.spec.js
@@ -2,10 +2,49 @@ import React from 'react';
 import toJson from 'enzyme-to-json';
 import { shallow } from 'enzyme';
 import '../../enzymeConfiguration';
+import { App, mapDispatchToProps, mapStateToProps } from '../../components/App';
 
-// todo
-describe('asd', function () {
-  it('should asd', function () {
-    expect(true).toBe(true);
+describe('App', function () {
+  it('should render', function () {
+    const props = {
+      graphSize: 2,
+    };
+    const component = shallow(<App {...props} />);
+    expect(toJson(component)).toMatchSnapshot();
+  });
+
+  it('should render with small graphSize', function () {
+    const props = {
+      graphSize: 2,
+    };
+    const component = shallow(<App {...props} />);
+    expect(component.find('.container')).toHaveLength(1);
+    expect(component.find('.container-fluid')).toHaveLength(0);
+  });
+
+  it('should render with big graphSize', function () {
+    const props = {
+      graphSize: 22,
+    };
+    const component = shallow(<App {...props} />);
+    expect(component.find('.container-fluid')).toHaveLength(1);
+  });
+});
+
+describe('mapStateToProps', function () {
+  it('should get sate', function () {
+    const state = {
+      graphSize: 3,
+    };
+    const result = mapStateToProps(state);
+    expect(result).toEqual(state);
+  });
+});
+
+describe('mapDispatchToProps', function () {
+  it('should get dispatch', function () {
+    const dispatch = () => {};
+    const result = mapDispatchToProps(dispatch);
+    expect(result).toEqual({});
   });
 });
--- src/__tests__/components/Header.spec.js
+++ src/__tests__/components/Header.spec.js
@@ -4,12 +4,8 @@ import { shallow } from 'enzyme';
 import '../../enzymeConfiguration';
 import { Header } from '../../components/Header';
 
-describe('Home', function () {
+describe('Header', function () {
   it('should render', function () {
-    const props = {
-      matrix: ['matrix'],
-      graphSize: 2,
-    };
     const component = shallow(<Header/>);
     expect(toJson(component)).toMatchSnapshot();
   });
